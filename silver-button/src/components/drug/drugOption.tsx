22e591ea71735d49593774fe9e88b5bea4593a7e
jaewon0615 <ujung8926@naver.com>
2024-12-26 12:24:53 +0900

    20241226

---
+++ silver-button/src/components/drug/drugOption.tsx
@@ -0,0 +1,63 @@
+/** @jsxImportSource @emotion/react */
+import React, { useState } from "react";
+import * as s from "../../styles/style";
+import {colorShape} from "./drugOprionData";
+
+// 약품 검색 옵션 상태 관리 (useState)
+interface SelectedDrugOption {
+  shape: string;
+  color: string;
+  line: string;
+}
+
+const optionInitialData: SelectedDrugOption = {
+  shape: '전체',
+  color: '전체',
+  line: '전체'
+}
+
+// const selectedDrugOption: SelectedDrugOption = {
+//   shape:drugShape,
+//   color:colorShape,
+//   line:lineShape
+// }
+
+// const optionInitialization: SelectedDrugOption = {
+//   shape: {}
+// }
+
+export default function Index01() {
+  const [selectedDrugOption, setSelectedDrugOption] = useState<SelectedDrugOption>(optionInitialData);
+
+
+  const handleColorShapeClick = (selectedDrugOption: SelectedDrugOption) => {
+    setSelectedDrugOption(selectedDrugOption);
+  };
+
+  
+  return (
+    <>
+      <div css={s.csearchSt}>
+        <div css={s.colorLt}>
+          {colorShape.map((shape) => (
+            <div
+              key={shape.id}
+              onClick={() => handleColorShapeClick(selectedDrugOption)}
+              css={shape.style} 
+            >
+              {shape.name}
+            </div>
+          ))}
+          <hr />
+          <div>
+            {selectedDrugOption && (
+              <div>
+                선택한 색상: {selectedDrugOption.color} / 선택한 모양: {selectedDrugOption.shape} / 선택한 분할선:{selectedDrugOption.line}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
