008106aeffef6a10874e6d34069a4997a4f1180f
sanggi9900 <tkdrl9900@gmail.com>
2025-01-02 10:32:41 +0900

    20250102 hsg .git/index

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Calendar from './views/Calendar/Calendar'
 import Authentication from './views/Authentication'
+import Board from './views/board'
+import CreatePost from './views/board/CreatePost'
+import PostDetail from './views/board/PostDetail'
+import EditPost from './views/board/EditPost'
 
 
 export default function App() {
@@ -9,6 +13,12 @@ export default function App() {
     <Routes>
           <Route path='/calendar' element={<Calendar />} />
           <Route path='/auth' element={<Authentication />} />
+          
+          <Route path="/board" element={<Board />} />
+          <Route path="/board/create" element={<CreatePost />} />
+          <Route path="/board/:id" element={<PostDetail />} />
+          <Route path="/board/:id/edit" element={<EditPost />} />
+          
     </Routes>
   )
 }
--- src/views/Authentication/SignIn/index.tsx
+++ src/views/Authentication/SignIn/index.tsx
@@ -75,7 +75,7 @@ export default function SignIn() {
     }
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/v1/auth/login",
+        "http://localhost:4040/api/v1/auth/login",
         credentials
       );
       if (response.data) {
