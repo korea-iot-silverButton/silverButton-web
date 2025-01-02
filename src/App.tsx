78ea473cd9fcf366ed74e29d94bc748044cb6eaa
jaewon0615 <ujung8926@naver.com>
2025-01-02 11:10:17 +0900

    20250102 jjw

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,29 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Calendar from './views/Calendar/Calendar'
-import Authentication from './views/Authentication'
-import MedicineSearch from './views/drug/medicineSearch'
-import MedicineSearchList from './views/drug/medicineListPage'
-import MedicineDetailPage from './views/drug/medicineDetailPage'
-import Board from './views/board'
-import CreatePost from './views/board/CreatePost'
-import PostDetail from './views/board/PostDetail'
-import EditPost from './views/board/EditPost'
-import Index from './views/drug/medicineSearch'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Calendar from "./views/Calendar/Calendar";
+import Authentication from "./views/Authentication";
+import MedicineSearch from "./views/drug/medicineSearch";
+import MedicineSearchList from "./views/drug/medicineListPage";
+import MedicineDetailPage from "./views/drug/medicineDetailPage";
+import Board from "./views/board";
+import CreatePost from "./views/board/CreatePost";
+import PostDetail from "./views/board/PostDetail";
+import EditPost from "./views/board/EditPost";
 
 export default function App() {
   return (
     <Routes>
-          <Route path='/calendar' element={<Calendar />} />
-          <Route path='/auth' element={<Authentication />} />
-          
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/auth" element={<Authentication />} />
 
+      <Route path="/medicineSearch" element={<MedicineSearch />} />
+      <Route path="/medicineListPage" element={<MedicineSearchList />} />
+      <Route path="/medicineDetailPage" element={<MedicineDetailPage />} />
 
-
-
-
-
-
-
-
-
-
-          <Route path='/medicineSearch' element={<MedicineSearch />} />
-          <Route path='/medicineListPage' element={<MedicineSearchList />} />
-          <Route path='/medicineDetailPage' element={<MedicineDetailPage />} />
-
-
-          <Route path="/board" element={<Board />} />
-          <Route path="/board/create" element={<CreatePost />} />
-          <Route path="/board/:id" element={<PostDetail />} />
-          <Route path="/board/:id/edit" element={<EditPost />} />
-          
-          <Route path='/medicinesearch' element={<Index />} />
-
+      <Route path="/board" element={<Board />} />
+      <Route path="/board/create" element={<CreatePost />} />
+      <Route path="/board/:id" element={<PostDetail />} />
+      <Route path="/board/:id/edit" element={<EditPost />} />
     </Routes>
-  )
+  );
 }
