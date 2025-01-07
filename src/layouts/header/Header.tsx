b037b17e6face780620c29ae2b4ee1af0806a096
devgiants75 <devgiants75@naver.com>
2025-01-07 20:11:57 +0900

    20240107 layouts

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,17 +1,17 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
-import React, { useState, useEffect } from "react";
-import { Box, Button, Typography } from "@mui/material";
+
+import React, { useEffect } from "react";
+import { Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import useAuthStore from "../../stores/auth.store";
 import SideBar from "../sideBar/SideBar";
-import mainImg from "./mainImg.png";
 
-export default function Header() {
-  // * state * //
+function HeaderToolWrap() {
   const { isAuthenticated, user, logout } = useAuthStore();
   const [cookies, setCookies] = useCookies(["token"]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!cookies.token) {
@@ -19,18 +19,10 @@ export default function Header() {
     }
   }, [cookies.token, logout]);
 
-
-  // Event handler for logout
   const handleLogOutClick = () => {
     setCookies("token", "", { expires: new Date() });
     logout();
     alert("로그아웃 되었습니다.");
-  };
-
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    navigate("/auth");
   };
 
   const handleNavigate = () => {
@@ -38,63 +30,56 @@ export default function Header() {
   };
 
   return (
+    <div css={s.headerToolWrap}>
+      <div css={s.headerLogo}>
+        <img src="/logo.png" alt="icon" css={s.headerLogoImg} />
+      </div>
+
+      <div css={s.headerToolKit}>
+        {isAuthenticated ? (
+          <>
+            <div>{user && <>{user.nickname}님 안녕하세요</>}</div>
+            <button onClick={handleLogOutClick} css={s.headerButton}>
+              로그아웃
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to={"/auth"} style={{ textDecoration: "none" }}>
+              <button css={s.headerButton}>
+                로그인
+              </button>
+            </Link>
+            <Link to={"/auth/signup"} style={{ textDecoration: "none" }}>
+              <button css={s.headerButton}>
+                회원가입
+              </button>
+            </Link>
+            <Link to={"/"} style={{ textDecoration: "none" }}>
+              <button css={s.headerButton}>
+                홈 화면
+              </button>
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function HeaderNaviWrap() {
+  return (
+    <div css={s.headerNaviWrap}>
+      <SideBar />
+    </div>
+  );
+}
+
+export default function Header() {
+  return (
     <div css={s.headerContianer}>
-      <div css={s.headerAll}>
-      <div css={s.headerDt}>
-      <div css={s.logoButton}>
-        <SideBar />
-        {/* 중앙 로고 */}
-        
-
-        <img
-          src={require("./메인로고2-removebg-preview.png")}
-          alt="icon"
-          css={s.img}
-        />
-      </div>
-      </div>
-      <Box>
-      {/* Light 버튼과 로그인/회원가입 - 상단 배경색 */}
-      <Box display="flex" justifyContent="space-between" alignItems="center" sx={{ paddingLeft: 50, paddingRight: 50,  p: 0.5 }}>
-
-      <div css={s.headerDt}>
-        <Box display="flex" alignItems="center">
-          {/* 로그인/회원가입 상태 */}
-          {isAuthenticated ? (
-            <>
-              <Typography variant="subtitle1" m={2}>
-                {user && <>{user.nickname}님 안녕하세요</>}
-              </Typography>
-              <Button variant="text" onClick={handleLogOutClick}>
-                로그아웃
-              </Button>
-            </>
-          ) : (
-            <>
-              <Link to={"/auth"} style={{ textDecoration: "none",  }}>
-                <Typography variant="subtitle1" m={2} css={s.button}>
-                  로그인
-                </Typography>
-              </Link>
-              <Link to={"/auth/signup"} style={{ textDecoration: "none", }}>
-                <Typography variant="subtitle1" m={2} css={s.button}>
-                  회원가입
-                </Typography>
-              </Link>
-          <button onClick={handleNavigate} css={s.button}>
-            HOME
-          </button>
-            </>
-          )}
-          {/* <div css={s.headerButton}> */}
-        {/* </div> */}
-        </Box>
-      </div>
-      </Box>
-      </Box>
-      </div>
-
-      
+      <HeaderToolWrap />
+      <HeaderNaviWrap />
     </div>
   );
 }
--- src/layouts/rootLayout/RootLayout.tsx
+++ src/layouts/rootLayout/RootLayout.tsx
@@ -1,18 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import * as s from './style'
-import React from 'react'
+import * as s from "./style";
+import React from "react";
 
-interface RootLayoutProps  {
-  children: React.ReactNode;  // children 속성 타입을 추가
-};
+interface RootLayoutProps {
+  children: React.ReactNode; // children 속성 타입을 추가
+}
 
-export default function RootLayout({ children } : RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <div css={s.fullDiv}>
-
-
-    {/* children은 동영상 위에 렌더링 */}
-    {children}
-  </div>
-  )
+      {/* children은 동영상 위에 렌더링 */}
+      {children}
+    </div>
+  );
 }
