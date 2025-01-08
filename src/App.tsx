604ccf66808ed0292baa41201559e63f371dde0d
guswl458@gmail.com <guswl458@gmail.com>
2025-01-08 17:46:44 +0900

    20250108

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ import Footer from "./layouts/footer/Footer";
 import HeaderLayout from "./layouts/headerLayout/HeaderLayout";
 import FooterLayout from "./layouts/footerLayout/FooterLayout";
 import MainLayout from "./layouts/mainLayout/MainLayout";
-import SignUp from "./views/authentication/signUp";
 import MessageBox from "./views/message/MessageBox";
 import MessageDetails from "./views/message/MessageDetails";
 
