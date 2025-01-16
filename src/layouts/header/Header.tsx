beb4bf3713cb6ab8cd6b802346e522ec6d433631
goshek <gsh6920@naver.com>
2025-01-16 15:12:48 +0900

    affa3w453af

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import useAuthStore from "../../stores/auth.store";
@@ -11,11 +11,17 @@ import { AiOutlineMedicineBox } from "react-icons/ai";
 import { LuClipboardPenLine } from "react-icons/lu";
 import { RiHealthBookLine } from "react-icons/ri";
 import { RiCalendarTodoLine } from "react-icons/ri";
+import axios from "axios";
 
 function HeaderToolWrap() {
   const { isAuthenticated, user, logout, login } = useAuthStore();
   const [cookies, setCookies] = useCookies(["token"]);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
+
+  const [editUser, setEditUser] = useState({
+    profileImg: user?.profileImg || "/images/profile.png", // 기본 이미지 경로 설정
+  });
 
   useEffect(() => {
     if (!cookies.token) {
@@ -47,6 +53,39 @@ function HeaderToolWrap() {
     navigate("/");
   }
 
+  // 페이지 로드 시 프로필 이미지 조회 (useEffect 사용)
+  useEffect(() => {
+    const fetchProfileImg = async () => {
+      try {
+        const response = await axios.get("http://localhost:4040/api/v1/manage/profile-img", {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${cookies.token}` 
+          }
+        });
+    
+        console.log(response); // 응답 내용 확인
+    
+        if (response.data && response.data.data) {
+          setEditUser((prev) => ({
+            ...prev,
+            profileImg: response.data.data,
+          }));
+        } else {
+          setEditUser((prev) => ({
+            ...prev,
+            profileImg: "/images/profile.png",
+          }));
+        }
+      } catch (err) {
+        console.error("프로필 이미지 조회 중 오류 발생:", err);
+        setError("프로필 이미지를 조회하는 데 오류가 발생했습니다.");
+      }
+    };
+
+    fetchProfileImg(); // API 호출
+  }, []);
+
 
   return (
     <div css={s.headerToolWrap}>
@@ -59,6 +98,15 @@ function HeaderToolWrap() {
           <>
             <div>{user && (
                 <>
+                <img
+              src={editUser.profileImg} // 수정된 프로필 이미지 반영
+              alt="Profile"
+              style={{
+                width: "20%",
+                height: "auto",
+                borderRadius: "100%",
+              }}
+            />
                   <img
                     src={getRoleImage(user.role)} // role에 맞는 이미지 출력
                     alt={user.role}
